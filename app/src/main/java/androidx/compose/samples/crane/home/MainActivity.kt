7cac534ab134fbe6c4b65605a3eec87f575bc6fb
DaffaMuflih <daffa.muflih02@gmail.com>
2025-03-10 14:09:48 +0700

    Step 5 : LaunchedEffect dan rememberUpdatedState

--- app/src/main/java/androidx/compose/samples/crane/home/MainActivity.kt
+++ app/src/main/java/androidx/compose/samples/crane/home/MainActivity.kt
@@ -17,11 +17,14 @@
 package androidx.compose.samples.crane.home
 
 import android.os.Bundle
+import androidx.compose.runtime.*
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.samples.crane.details.launchDetailsActivity
 import androidx.compose.samples.crane.ui.CraneTheme
 import androidx.core.view.WindowCompat
@@ -51,6 +54,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 private fun MainScreen(onExploreItemClicked: OnExploreItemClicked) {
     Surface(color = MaterialTheme.colors.primary) {
-        CraneHome(onExploreItemClicked = onExploreItemClicked)
+        var showLandingScreen by remember { mutableStateOf(true) }
+        if (showLandingScreen) {
+            LandingScreen(onTimeout = { showLandingScreen = false })
+        } else {
+            CraneHome(onExploreItemClicked = onExploreItemClicked)
+        }
     }
 }
